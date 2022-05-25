38c86454696f39be64e49703b52267bd50ac86e4
unknown <luuduc34@hotmail.com>
2022-05-25 11:34:43 +0200

    add4.2.js

--- 4.2.js
+++ 4.2.js
@@ -3,4 +3,7 @@ const readlineSync = require("readline-sync");
 function rand10() {
     return Math.floor(Math.random() * 10) + 1;
 }
-console.log()
+let n = readlineSync.question('How many random numbers do you want ? : ');
+for(let i = 1; i <= n; i++) {
+    console.log('Random number ' + i + ' is : ' + rand10());
+}
