65d4891e47bdae9d0823a8015ca0ddd305a53479
unknown <luuduc34@hotmail.com>
2022-05-25 11:59:17 +0200

    add documentation of random func

--- 4.2.js
+++ 4.2.js
@@ -7,3 +7,14 @@ let n = readlineSync.question('How many random numbers do you want ? : ');
 for(let i = 1; i <= n; i++) {
     console.log('Random number ' + i + ' is : ' + rand10());
 }
+
+/* Documentation of random function
+------------------------------------
+// Returns a random number (always returns a number lower than 1):
+Math.random();
+
+Math.random() used with Math.floor() can be used to return random integers.
+// Returns a random integer from 0 to 9:
+Math.floor(Math.random() * 10);
+// Returns a random integer from 1 to 10:
+Math.floor(Math.random() * 10) + 1; */
